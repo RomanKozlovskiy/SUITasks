958925097d137a6b19c8ce7a0fd321b9e66e15c7
RomanKozlovskiy <roman.kozlovsky@fojin.tech>
2024-06-13 11:03:36 +0300

    common project settings

---
+++ SUITasks/SUITasks/SUITasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUITasksApp.swift
+//  SUITasks
+//
+//  Created by user on 13.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SUITasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
